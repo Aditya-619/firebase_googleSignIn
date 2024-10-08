663d70e668557f31c360b52f4cd267180cb25149
Aditya-619 <ranjanaditya5678@gmail.com>
2024-10-08 18:03:49 +0530

    fixes

--- client/src/config/firebase.js
+++ client/src/config/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyC1opFI4zcTYTemwcCAyl4j7r5KVniuIlI",
+    authDomain: "backendtesting-cad28.firebaseapp.com",
+    projectId: "backendtesting-cad28",
+    storageBucket: "backendtesting-cad28.appspot.com",
+    messagingSenderId: "394104713073",
+    appId: "1:394104713073:web:8448a9b1111663c86c90d0",
+    measurementId: "G-N24H787Z0F"
 };
-e
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
